baef534165d3510b35362034d2582e9ad85d9381
SohanX_Mathiya <130471209+KingSohanZ@users.noreply.github.com>
2024-07-22 05:18:44 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,9 @@
-
+from flask import Flask, render_template
+from flask_cors import CORS 
+app = Flask(_name_)
+CORS(app)
+@app.router('/')
+def home():
+  return render_template('index.html)
+if _name_ == '_main_':
+       app.run(debug=True)            
